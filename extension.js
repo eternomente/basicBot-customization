7c629befcae91e450ac5e2b197d1b008a28d3659
BotFarofa <huehukakde3@hotmail.com>
2015-07-29 01:28:23 -0300

    Update extension.js

--- extension.js
+++ extension.js
@@ -35,14 +35,14 @@
          */
 
         bot.commands.baconCommand = {
-            command: ['kaz', 'fu'],  //The command to be called. With the standard command literal this would be: !bacon
+            command: ['robo', '22'],  //The command to be called. With the standard command literal this would be: !bacon
             rank: 'user', //Minimum user permission to use the command
             type: 'exact', //Specify if it can accept variables or not (if so, these have to be handled yourself through the chat.message
             functionality: function (chat, cmd) {
                 if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
                 if (!bot.commands.executable(this.rank, chat)) return void (0);
                 else {
-                    API.sendChat("/me BAN!?!?!?");
+                    API.sendChat("/me o @Robot8bit vai te sentar o 22");
                 }
             }
         };
