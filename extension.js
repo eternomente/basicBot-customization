48d10f723e0c131c4682d87e5615b461458def37
Farofa Pop <huehukakde3@hotmail.com>
2016-06-04 18:55:05 -0300

    Update extension.js

--- extension.js
+++ extension.js
@@ -113,6 +113,6 @@
     }));
 
     //Start the bot and extend it when it has loaded.
-    $.getScript("https://rawgit.com/BotFarofa/Farofa-Bot/master/basicBot.js", extend);
+    $.getScript("https://rawgit.com/basicBot/source/master/basicBot.js", extend);
 
 }).call(this);
